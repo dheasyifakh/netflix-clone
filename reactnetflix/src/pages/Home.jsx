d494dbc05b22ec37513943b2e2c77ea95777a129
Dhea Syifa Khoirunnisa <dheasyifa.khoirunnisa@spentera.com>
2024-09-13 13:40:19 +0700

    Add Framer Motion Library

--- reactnetflix/src/pages/Home.jsx
+++ reactnetflix/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 import { languageAtom } from '../jotai/atoms';
 import { useEffect, useState } from 'react';
 import { apiInstance } from '../utils/apiInstance';
-
+import {motion} from 'framer-motion';
 function Home(){
   const[language, ] = useAtom(languageAtom)
   const [movies, setMovies] = useState(null)
@@ -20,22 +20,30 @@ function Home(){
   
   return(
     <Layout>
-      <h1 className='text-center p-12'>{language == 'en' ? "Watch Netflix Free!": "Tonton Netflix Gratis!"}</h1>
-      <p className='bg-black text-white p-4 text-center'>Message from API: {movies?.message}</p>
-      <div className="card bg-base-100 w-96 shadow-xl">
-  <figure className='mt-5'>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+      <motion.h1 
+        initial={{ x: "100%", opacity: 0}}
+        animate={{ x: "0%", opacity: 1}}
+        transition={{ duration: 1, ease: "backOut"}}
+        className='text-center p-6 text-3xl'>{language == 'en' ? "Watch Netflix Free!": "Tonton Netflix Gratis!"}</motion.h1>
+      <p className='text-center text-indigo-500 font-bold text-sm p-4'>Message from API: {movies?.message}</p>
+      <motion.div 
+        initial={{ y: "100%", opacity: 0}}
+        animate={{ y: "0%", opacity: 1}}
+        transition={{ duration: 1, ease: "backOut"}}
+        className="grid grid-cols-2 gap-4">
+        <div className="bg-indigo-500 p-4">
+          <h1>Hello World</h1>
+        </div>
+        <div className="bg-indigo-500 p-4">
+          <h1>Hello World 2</h1>
+        </div>
+        <div className="bg-indigo-500 p-4">
+          <h1>Hello World 3</h1>
+        </div>
+        <div className="bg-indigo-500 p-4">
+          <h1>Hello World 4</h1>
+        </div>
+      </motion.div>
     </Layout>
   )
 
